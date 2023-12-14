3cb0b8f29d737f42f9611e55dc41a3ffa78c2b8a
Kaue Siqueira Reis <kauesiqueira_@hotmail.com>
2023-12-14 13:02:35 -0300

    feat: create function Search by Id

--- FilmesApi/Controllers/FilmeController.cs
+++ FilmesApi/Controllers/FilmeController.cs
@@ -9,10 +9,12 @@ public class FilmeController : ControllerBase
 {
 
     private static List <Filme> filmes = new List<Filme> ();
+    private static int id = 0;
 
     [HttpPost]
     public void AdicionarFilme([FromBody] Filme filme)
     {
+        filme.Id = id++;
         filmes.Add(filme);
         Console.WriteLine(filme.Titulo);
         Console.WriteLine(filme.Duracao);
@@ -22,6 +24,12 @@ public class FilmeController : ControllerBase
     public IEnumerable<Filme> RecuperarFilmes()
     {
         return filmes;
+    }
+
+    [HttpGet("{id}")]
+    public Filme? RecuperaFilmePorId(int id)
+    {
+        return filmes.FirstOrDefault(filme => filme.Id == id);
     }
 
 }
