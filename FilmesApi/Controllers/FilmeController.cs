27d0241eb656f1adf3ccdbbafeb67926fd340a7f
Kaue Siqueira Reis <kauesiqueira_@hotmail.com>
2023-12-14 20:01:38 -0300

    feat: create function Delete

--- FilmesApi/Controllers/FilmeController.cs
+++ FilmesApi/Controllers/FilmeController.cs
@@ -30,9 +30,9 @@ public class FilmeController : ControllerBase
     }
 
     [HttpGet]
-    public IEnumerable<Filme> RecuperarFilmes([FromQuery] int skip = 0,[FromQuery] int take = 10)
+    public IEnumerable<ReadFilmeDto> RecuperarFilmes([FromQuery] int skip = 0,[FromQuery] int take = 10)
     {
-        return _context.Filmes.Skip(skip).Take(take);
+        return _mapper.Map<List<ReadFilmeDto>>(_context.Filmes.Skip(skip).Take(take));
     }
 
     [HttpGet("{id}")]
@@ -40,7 +40,10 @@ public class FilmeController : ControllerBase
     {
         var filme = _context.Filmes.FirstOrDefault(filme => filme.Id == id);
         if (filme == null) return NotFound();
-        return Ok(filme);
+
+        var filmeDto = _mapper.Map<ReadFilmeDto>(filme);   
+        
+        return Ok(filmeDto);
     }
 
     [HttpPut("{id}")]
@@ -70,6 +73,18 @@ public class FilmeController : ControllerBase
         }
 
         _mapper.Map(filmeParaAtualizar, filme);
+        _context.SaveChanges();
+        return NoContent();
+    }
+
+    [HttpDelete("{id}")]
+    public IActionResult DeletaFilme(int id)
+    {
+        var filme = _context.Filmes.FirstOrDefault(filme => filme.Id == id);
+
+        if (filme == null) return NotFound();
+
+        _context.Remove(filme);
         _context.SaveChanges();
         return NoContent();
     }
