8ae098a5ceadb7dc0b65fec4fcca013e0b5553d2
Kaue Siqueira Reis <kauesiqueira_@hotmail.com>
2023-12-14 12:55:10 -0300

    feat: create Class Filme

--- FilmesApi/Models/Filme.cs
+++ FilmesApi/Models/Filme.cs
@@ -1,9 +1,18 @@
-﻿namespace FilmesApi.Models;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FilmesApi.Models;
 
 public class Filme
 {
+    [Key]
+    [Required]
     public int Id { get; set; }
-    public string Titlulo { get; set; }
+    [Required(ErrorMessage = "O Título do Filme é obrigatório!")]
+    public string Titulo { get; set; }
+    [Required(ErrorMessage = "O Gênero do Filme é obrigatório!")]
+    [MaxLength(50, ErrorMessage = "O tamanho do Gênero não pode exceder 50 caracteres.")]
     public string Genero { get; set; }
+    [Required]
+    [Range(70, 600, ErrorMessage = "A duração deve ser entre 70 e 600 minutos.")]
     public int Duracao { get; set; }
 }
